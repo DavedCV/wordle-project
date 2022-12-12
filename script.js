970a34cd52ac12c06ff00763c76f61c33686a2ce
David Castrillón <97773641+Davedcod@users.noreply.github.com>
2022-12-12 17:29:24 -0500

    addition of some javascript to add event listeners to the div, in order to read the keystrokes

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function makeSquares(){
     for (let row of rows) {
+        let childs = [];
         for (let i = 0; i < 5; i++){
             let square = document.createElement("div");
             square.classList.add("square");
             row.appendChild(square);
+            childs.push(square);
         }
+        rowsChilds.push(childs);
     }
 
 }
 
+function keyPressed(ev){
+    console.log("keyPressed");
+}
+
+//selector to query the divs with class rows
 const rows = document.querySelectorAll('.row');
+let rowsChilds = [];
 makeSquares();
+
+const container = document.getElementsByClassName('container')[0];
+container.addEventListener('click',() => {container.focus()});
+container.addEventListener('keydown', keyPressed);
