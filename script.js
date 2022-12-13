ea0d5667497a7620bb83f4b104e1ba4a9221b2da
David Castrillón <97773641+Davedcod@users.noreply.github.com>
2022-12-13 18:19:04 -0500

    avance in the srcipt and logic of the program

--- script.js
+++ script.js
@@ -8,33 +8,33 @@ function makeSquares(){
             childs.push(square);
         }
         rowsChilds.push(childs);
+        row.addEventListener("animationend", (e) => {
+            e.target.classList.remove("invalid");
+        })
     }
 
 }
 
 function keyPressed(ev){
-    let numberOfSquare = squareCounter % 5;
-    let numberOfRow = Math.floor(squareCounter / 5);
-    if (isLetter(ev.key) && ev.code != "Enter" && ev.code != "Backspace"){
-        rowsChilds[numberOfRow][numberOfSquare].textContent = ev.key;
-        if (numberOfSquare == 4){
+    if (isLetter(ev.key)){
+        rowsChilds[0][squareCounter].textContent = ev.key;
+        if (squareCounter == 4){
             (actualWord.length == 5) ? actualWord = actualWord.slice(0,-1) + ev.key : actualWord += ev.key;
             return false;
         }
         actualWord += ev.key;
         return true;
     }else if (squareCounter >= 0 && ev.code == "Backspace"){
-        if (rowsChilds[numberOfRow][numberOfSquare].textContent != ""){
-            rowsChilds[numberOfRow][numberOfSquare].textContent = "";
+        if (rowsChilds[0][squareCounter].textContent != ""){
+            rowsChilds[0][squareCounter].textContent = "";
         }else{
             (squareCounter != 0) ? squareCounter-- : squareCounter = 0;
-            numberOfSquare = squareCounter % 5;
-            numberOfRow = Math.floor(squareCounter / 5);
-            rowsChilds[numberOfRow][numberOfSquare].textContent = ""; 
+            squareCounter = squareCounter % 5;
+            rowsChilds[0][squareCounter].textContent = ""; 
         }
         actualWord = actualWord.slice(0,-1);
         return false;   
-    }else if (numberOfSquare == 4 && ev.code == "Enter" && rowsChilds[numberOfRow][numberOfSquare].textContent != ""){
+    }else if (squareCounter == 4 && ev.code == "Enter" && rowsChilds[0][squareCounter].textContent != ""){
         const promise = apiConfirmation(actualWord);
         actualWord = "";
         return true;
@@ -42,7 +42,14 @@ function keyPressed(ev){
 }
 
 function compare(word){
-    console.log("parametro: ", word);
+    let copySelectedWord = selectedWord;
+    for (let i = 0; i < word.length; i++){
+        if (copySelectedWord.indexOf(word[i]) != -1){
+            (copySelectedWord.indexOf(word[i]) === i) ? rowsChilds[0][i].classList.add("correct") :
+            rowsChilds[0][i].classList.add("close");
+            copySelectedWord = selectedWord.replace(word[i], "-");
+        }
+    }
 }
 
 function isLetter(letter) {
@@ -50,23 +57,33 @@ function isLetter(letter) {
 }
 
 async function apiConfirmation(word){
+    infoBarSpiral.classList.remove("hidden");
     const promise = await fetch("https://words.dev-apis.com/validate-word", {
         method: "POST",
         body: JSON.stringify({"word": word})
     })
+    infoBarSpiral.classList.add("hidden");
     const processedResponse = await promise.json();
-    console.log("en la funcion asincrona");
     if (processedResponse.validWord === true){
         compare(word);
+        rowsChilds.shift();
+        squareCounter = (squareCounter + 1) % 6;
+    }else{
+        squareCounter--;
+        let rowNumber = Math.abs(rowsChilds.length-rows.length);
+        rows[rowNumber].classList.add("invalid");
     }
 }
 
 async function apiRandomWord(){
-    const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
+    const promise = await fetch("https://words.dev-apis.com/word-of-the-day?random=1");
     const processedResponse = await promise.json();
-    console.log("random word: ", processedResponse.word);
     selectedWord = processedResponse.word;
+    console.log("random word: ", selectedWord);
+    infoBarSpiral.classList.add("hidden");
 }
+
+const infoBarSpiral = document.querySelector(".info-bar");
 
 let selectedWord;
 apiRandomWord();
@@ -83,6 +100,6 @@ let actualWord = "";
 let squareCounter = 0;
 container.addEventListener('keydown', (ev) => {
     if (keyPressed(ev)){
-        squareCounter++;
+        squareCounter = (squareCounter + 1) % 6;
     }
 });
